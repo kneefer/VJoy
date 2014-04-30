30568542727c3061b809bb21b8dff8dd46ef09ab
Szymon - Desktop <szymbar099@student.polsl.pl>
2014-04-30 15:30:20 +0200

    added missing files

---
+++ VJoyTestFeeder/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using VJoyTestFeeder.VJoyServiceReference;
+
+namespace VJoyTestFeeder
+{
+    public partial class MainWindow
+    {
+        private ServiceClient _client;
+        public int JoyId { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _client = new ServiceClient();
+            JoyId = _client.ConnectJoystick();
+
+            DataContext = this;
+        }
+
+
+    }
+}
